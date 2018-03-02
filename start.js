6b3e47012922ec21c60c980a2cf8001696e568ed
José Paulo Bernardes <8150148@estg.ipp.pt>
2018-03-02 16:01:42 +0000

    apagar lixo

--- start.js
+++
@@ -1,6 +0,0 @@
-var copyfiles = require('copyfiles');
-console.log(__dirname);
-copyfiles(['../src/public/**/*', '../src/views/**/*', './'], 2, () => {
-    console.log("Files copied");
-});
-var aaa = require('./dist/server.js');
