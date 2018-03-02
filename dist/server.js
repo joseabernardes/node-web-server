4a3aa71c5dda26ed99b3dd63f40a8e19c76e08e1
José Paulo Bernardes <8150148@estg.ipp.pt>
2018-03-02 15:22:16 +0000

    cenas para o heroku

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
+const port = process.env.PORT || 3000;
 const app = express();
+console.log(__dirname);
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getYear', () => {
     return new Date().getFullYear();
@@ -29,7 +31,8 @@ app.use((req, res, next) => {
 // });
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs'); //configuarar o HBS
-// app.set('views', __dirname + '/views'); //conf a pasta views
+app.set('views', __dirname + '/views'); //conf a pasta views
+//wasedgfh
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express! 2</h1>');
     // res.send({
@@ -54,6 +57,6 @@ app.get('/bad', (req, res) => {
         errorMessage: "unable to error"
     });
 });
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import {NextFunction, Request, Response} from "express-serve-static-core";
 import * as hbs from 'hbs';
 import * as fs from 'fs';
 
+const port = process.env.PORT || 3000;
 const app = express();
-
+console.log(__dirname);
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getYear', () => {
     return new Date().getFullYear();
@@ -31,11 +32,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 // app.use((req: Request, res: Response, next: NextFunction) => {
 //     res.render('maintain.hbs');
 // });
+
 app.use(express.static(__dirname + '/public'));
 
 
 app.set('view engine', 'hbs');//configuarar o HBS
-// app.set('views', __dirname + '/views'); //conf a pasta views
+app.set('views', __dirname + '/views'); //conf a pasta views
+//wasedgfh
 
 app.get('/', (req: Request, res: Response) => {
     // res.send('<h1>Hello Express! 2</h1>');
@@ -67,8 +70,8 @@ app.get('/bad', (req: Request, res: Response) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
 
 
